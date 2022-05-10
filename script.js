1fae7b14c6828441f93451b40d12f722fa8f3083
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2022-05-10 11:34:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var Name = "daniel";
+var x = 5
+var y = 8
+const z = x + y;
+var phone1 = "nokia 2.2"
+var phone1b = "iphone 2+"
+var fontSize = "13cm"
+var width = "12rem"
+
+
+
+
+
+console.log("i am",Name)
+console.log(z)
+console.log(phone1)
+console.log(phone1b)
+console.log(fontSize)
+console.log(width)
